3fca6637d14764ddfedf625e06b6ce51f46849ec
nkca122 <nkca122@gmail.com>
2024-04-22 16:12:35 +0530

    APIs

--- JS Basics/app.js
+++ JS Basics/app.js
@@ -1,24 +1,11 @@
-let btn = document.querySelector("button");
-let ul = document.querySelector("ul");
-let inp = document.querySelector("input");
-
-btn.addEventListener("click", function() {
-    let item = document.createElement("li");
-    item.innerText = inp.value;
-
-    let delBtn = document.createElement("button");
-    delBtn.innerText = "Delete";
-    delBtn.classList.add("delete");
-
-    item.appendChild(delBtn);
-    ul.appendChild(item);
-
-    inp.value = "";
-});
-
-ul.addEventListener("click", function(event) {
-    if(event.target.nodeName = "BUTTON"){
-        let listitem = event.target.parentElement;
-        listitem.remove();
+const url = "https://catfact.ninja/fact";
+async function getFacts(){
+    try {
+        let resp = await axios.get(url);
+        console.log(resp.data.fact);
+    } catch(err) {
+        console.log(err);
     }
-})
+}
+
+getFacts();
